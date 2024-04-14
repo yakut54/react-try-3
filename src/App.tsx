0926bd89a870a2ee2f57c0cdf3df39b74c96e28b
yakut54 <mongodb1@yandex.ru>
2024-04-14 08:52:43 +0700

    Organization of styles. Adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React, {Component, Suspense} from 'react';
-import './index.scss';
+import React, {Suspense} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async"
+import {useTheme} from './theme/useTheme'
 
-export class App extends Component {
-    render() {
-        return (
-            <div className={'app'}>
-                <Link to={'/'}>Главная</Link>
-                <Link to={'/about'}>О проекте</Link>
 
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path={'/about'} element={<AboutPageAsync/>}/>
-                        <Route path={'/'} element={<MainPageAsync/>}/>
-                    </Routes>
-                </Suspense>
-            </div>
-        );
-    }
+export const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOOGLE</button>
+
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О проекте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
 }
 
 
