aaba52d6bee20a7c4b3282f4603b48b172c97502
yakut54 <mongodb1@yandex.ru>
2024-04-14 12:23:52 +0700

    create classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async"
 import {useTheme} from './theme/useTheme'
+import {classNames} from "./helpers/classNames/classNames";
 
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOOGLE</button>
 
             <Link to={'/'}>Главная</Link>
