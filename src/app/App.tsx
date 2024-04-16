dbb9bafe1cb5b45151d1b3e8307b5cbbb728c904
yakut54 <mongodb1@yandex.ru>
2024-04-16 08:00:48 +0700

    App Router. Configuration for the router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,8 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from "react-router-dom";
+import React from 'react';
+import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 import './styles/index.scss';
 
 
@@ -17,12 +16,7 @@ export const App = () => {
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О проекте</Link>
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 }
