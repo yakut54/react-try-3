dd3b6ead48a0d5029ca905b7ef5128e8d820c453
yakut54 <mongodb1@yandex.ru>
2024-04-17 13:23:56 +0700

    Navbar. The first UI Kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 import './styles/index.scss';
 
 
@@ -11,12 +11,9 @@ export const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOOGLE</button>
-
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О проекте</Link>
-
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOOGLE</button>
         </div>
     );
 }
