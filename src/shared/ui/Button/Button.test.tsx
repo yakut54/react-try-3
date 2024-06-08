f6bdba532e40941984e5ebff012200a93c068e24
yakut54 <mongodb1@yandex.ru>
2024-06-08 10:43:59 +0700

    Button tests

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react'
 import { Button, ButtonTheme } from 'shared/ui/Button/Button'
 
 describe('Button', () => {
-  test('Button In The Document', () => {
+  test('Test Render', () => {
     render(<Button>123</Button>)
     expect(screen.getByText('123')).toBeInTheDocument()
   })
 
-  test('Button To Have Class', () => {
+  test('Test Clear Theme', () => {
     render(<Button theme={ButtonTheme.CLEAR}>123</Button>)
     expect(screen.getByText('123')).toHaveClass('clear')
     screen.debug()
