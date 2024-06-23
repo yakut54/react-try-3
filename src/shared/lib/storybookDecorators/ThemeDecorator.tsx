db03a12102877b2b77442656d2ff809720879390
yakut54 <mongodb1@yandex.ru>
2024-06-23 18:29:31 +0700

    ThemeDecorator refactor

--- src/shared/lib/storybookDecorators/ThemeDecorator.tsx
+++ src/shared/lib/storybookDecorators/ThemeDecorator.tsx
@@ -1,10 +1,15 @@
-import { StoryFn } from '@storybook/react'
+import { StoryFn } from '@storybook/react/'
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider'
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => (
-  <ThemeProvider>
-    <div className={`flex-center app ${theme}`}>
-      <StoryComponent />
-    </div>
-  </ThemeProvider>
-)
+export const ThemeDecorator = (theme: Theme) => {
+  const DecoratedStoryComponent = (StoryComponent: StoryFn) => (
+    <ThemeProvider>
+      <div className={`flex-center app ${theme}`}>
+        <StoryComponent />
+      </div>
+    </ThemeProvider>
+  )
+
+  DecoratedStoryComponent.displayName = `ThemeDecorator(${theme})`
+  return DecoratedStoryComponent
+}
