31e08cfbd6b99071406160ede2455278536f62b4
yakut54 <mongodb1@yandex.ru>
2024-07-04 18:49:32 +0700

    Custom Input. Authorization window

---
+++ src/entities/User/index.ts
@@ -0,0 +1,7 @@
+export { userActions, userReducer } from './model/slice/userSlice'
+export type { User, UserSchema } from './model/types/userSchema'
+
+// сегодня 02.07.2024 ура!
+// export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData'
+// export { getUserInit } from './model/selectors/getUserInit/getUserInit'
+// export { getUserIsLoading } from './model/selectors/getUserIsLoading/getUserIsLoading'
