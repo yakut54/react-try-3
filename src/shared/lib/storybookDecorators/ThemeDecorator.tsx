cf5b46bed3f667edc29c6db5af80877be76ba4a2
yakut54 <mongodb1@yandex.ru>
2024-07-10 09:03:23 +0700

    petli n

--- src/shared/lib/storybookDecorators/ThemeDecorator.tsx
+++ src/shared/lib/storybookDecorators/ThemeDecorator.tsx
@@ -4,9 +4,7 @@ import { StateSchema, StoreProvider } from 'app/providers/StoreProvider'
 import { componentRenderOptions } from 'shared/lib/tests/RenderWithRouter'
 
 export const ThemeDecorator = (theme: Theme, options: componentRenderOptions = {}) => {
-  const DecoratedStoryComponent = (
-    StoryComponent: StoryFn,
-  ) => (
+  const DecoratedStoryComponent = (StoryComponent: StoryFn) => (
     <StoreProvider initialState={options.initialState as StateSchema}>
       <ThemeProvider initialTheme={theme}>
         <div className={`flex-center app ${theme}`}>
