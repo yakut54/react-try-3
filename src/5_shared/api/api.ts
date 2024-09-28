1fd7f250e8e0f2fa3b5ffcdfc82dd452163b00a5
yakut54 <mongodb1@yandex.ru>
2024-09-28 16:56:07 +0700

    $api.interceptors

--- src/5_shared/api/api.ts
+++ src/5_shared/api/api.ts
@@ -3,5 +3,12 @@ import { USER_LOCAL_STORAGE_KEY } from '5_shared/const/localStorage'
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: { Authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) },
+})
+
+$api.interceptors.request.use((config) => {
+  const newConfig = config
+  if (newConfig.headers) {
+    newConfig.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY)
+  }
+  return newConfig
 })
